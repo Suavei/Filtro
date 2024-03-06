dbd74f6407613c5fc7d0ff46023730488b8000f7
Vijay Soni <143437956+Suavei@users.noreply.github.com>
2024-03-06 16:29:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function change(){
+    var element = document.getElementById("section-second");
+    var width = window.innerWidth;
+    if(width < 1240){
+        element.innerHTML = '<h1 class="hr-line">&nbsp;&nbsp;Explore the Coffee World&nbsp;&nbsp;</h1><div class="container1"><div class="swiper"><div class="swiper-wrapper"><div class="swiper-slide"><div class="container"><div class="wrapper"><div class="banner-image banner"></div><h1>Cappacino</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,recusandae?</p><div class="price"><div class="amount">$8.60</div><button>Buy Now</button></div></div></div></div><div class="swiper-slide"><div class="container"><div class="wrapper"><div class="banner-image2 banner"></div><h1>Latte</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,recusandae?</p><div class="price"><div class="amount">$11.40</div><button>Buy Now</button></div></div></div></div><div class="swiper-slide"><div class="container"><div class="wrapper"><div class="banner-image3 banner"></div><h1>Mocha</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,recusandae?</p><div class="price"><div class="amount">$9.20</div><button>Buy Now</button></div></div></div></div><div class="swiper-slide"><div class="container"><div class="wrapper"><div class="banner-image4 banner"></div><h1>Cold Java</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,recusandae?</p><div class="price"><div class="amount">$6.40</div><button>Buy Now</button></div></div></div></div></div><div class="swiper-pagination"></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div></div>'
+    }else{
+        element.innerHTML = '<h1 class="hr-line">&nbsp;&nbsp;Explore the Coffee World&nbsp;&nbsp;</h1> <div class="main"> <div class="container"> <div class="wrapper"> <div class="banner-image banner"></div> <h1>Cappacino</h1> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, recusandae? </p> <div class="price"> <div class="amount">$8.60</div> <button>Buy Now</button> </div> </div> </div> <div class="container"> <div class="wrapper"> <div class="banner-image2 banner"></div> <h1>Latte</h1> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, recusandae? </p> <div class="price"> <div class="amount">$11.40</div> <button>Buy Now</button> </div> </div> </div> <div class="container"> <div class="wrapper"> <div class="banner-image3 banner"></div> <h1>Mocha</h1> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, recusandae? </p> <div class="price"> <div class="amount">$9.20</div> <button>Buy Now</button> </div> </div> </div> <div class="container"> <div class="wrapper"> <div class="banner-image4 banner"></div> <h1>Cold Java</h1> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, recusandae? </p> <div class="price"> <div class="amount">$6.40</div> <button>Buy Now</button> </div> </div> </div> </div>';
+    }
+
+    const swiper = new Swiper('.swiper', {
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+    
+        // If we need pagination
+         pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+    
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        });
+    
+}
+window.addEventListener('resize',change);
